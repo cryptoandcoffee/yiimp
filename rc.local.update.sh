69e7d2d24eb554bda5994f8415aaff70ff234bba
cryptoandcoffee <19512127+88plug@users.noreply.github.com>
2018-10-01 14:50:24 -0700

    Final fix for rc.local.update

--- rc.local.update.sh
+++ rc.local.update.sh
@@ -1,9 +1,11 @@
-#Generates an rc.local file that is correct for stratum algos
+#Generates rc.local file that is correct for stratum algos
 #!/bin/bash
+rm rc.local_stratum
 confs=( $(find ./stratum/config.sample/ -maxdepth 1 -printf '%P\n' | grep -v ".log" | grep -v ".sh" | sort) )
+echo "STRATUM_DIR=/var/stratum" > rc.local_stratum
 
 for conf in "${confs[@]}"; do
     suffix=".conf"
     algo=$(echo $conf | sed -e "s/$suffix$//" )
-    echo "screen -dmS $algo $STRATUM_DIR/run.sh $algo"
+    echo "screen -dmS $algo \$STRATUM_DIR/run.sh $algo" >> rc.local_stratum
 done
